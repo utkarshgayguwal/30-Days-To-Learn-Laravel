fed2caba130b196a73d0bfeee60d4a26505d6c1c
Utkarsh Gayguwal <utkarshgayguwal123@gmail.com>
2025-05-11 06:35:09 +0530

    Ep2: Your First Route and View

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -20,7 +20,7 @@
         @endif
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <h1>Hello, From Home Page</h1>
+        <h1>Hello, From About Page</h1>
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
